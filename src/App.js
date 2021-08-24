c147c53c2c6d3ab8a89054e9cf6780e2781fb903
Paulo Abrahão <ab.cod@outlook.com>
2021-08-24 11:25:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
+import githubImage from './githubImg.jpg'
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submit")
+  }
+  
+  const handleChange = (event) => {
+    // altera o valor escrito no campo
+    setSearch(event.target.value)
+
+    fetch(`https://api.github.com/users/${search}`)
+    .then(response => response.json())
+    .then(userResponse => setUserData(userResponse))
+  }
+  
+
+  console.log(userData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase"> Github profile</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group"> 
+         
+          <div className="input-group">
+            <input 
+              className="form-control"
+              type="text"
+              required
+              value={search}
+              onChange={handleChange}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+
+      <div className="py-5"> 
+        {!userData && (
+          <img src={githubImage} alt="" className="responsive rounded-circle"  height="150px"/> 
+        )}
+        {userData && (
+          <div>
+            <img src={userData.avatar_url} alt="" className="responsive rounded-circle"  height="150px"/> 
+            <h1 className="pt-4">
+              <a href="https://github.com/PauloAbrahao" target="_new"> 
+              {userData.name}
+              </a>
+            </h1>
+            <h3>
+              {userData.location}
+            </h3>
+          </div>
+        )}
+
+      </ div>
     </div>
   );
 }
